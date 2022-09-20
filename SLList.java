bb7b2ed29f8571bd842a3e987725e5f787790fa6
Zahi <1737453862@qq.com>
2022-09-20 11:39:56 +0800

    SLList update

--- SLList.java
+++ SLList.java
@@ -1,30 +1,42 @@
-public class SLList {
-    private class IntNode{
-        public int item;
-        public IntNode next;
+public class SLList<Item> implements List61B<Item> {
+    private class ListNode{
+        public Item item;
+        public ListNode next;
         
-        public IntNode(int item, IntNode next){
+        public ListNode(Item item, ListNode next){
             this.item = item;
             this.next = next;
         }
     }
 
-    private IntNode first;
+    private ListNode first;
 
-    public SLList(IntNode first){
+    public SLList(ListNode first){
         this.first = first;
     }
 
-    public void addFirst(int x){
-        first = new IntNode(x, first);
+    public void addFirst(Item x){
+        first = new ListNode(x, first);
     }
 
-    public void insert(int item, int position){
+    public void addLast(Item y){
+        if (this.first == null){
+            first = new ListNode(y, first);
+            return;
+        }
+        ListNode p = this.first;
+        while (p.next != null){
+            p = p.next;
+        }
+        p.next = new ListNode(y, p.next);
+    }
+
+    public void insert(Item item, int position){
         if (position == 0){
             addFirst(item);
             return;
         }
-        IntNode p = first;
+        ListNode p = first;
         while(position != 1){
             p = p.next;
             position -= 1;
@@ -32,28 +44,28 @@ public class SLList {
                 break;
             }
         }
-        p.next = new IntNode(item, p.next);
+        p.next = new ListNode(item, p.next);
     }
 
     public void reverse(){
         if (first.next == null){
             return;
         }
-        IntNode p = first.next;
+        ListNode p = first.next;
         first.next = null;
         if (p.next == null){
             p.next = first;
             first = p;
             return;
         }
-        IntNode q = p.next;
+        ListNode q = p.next;
         p.next = first;
         if (q.next == null){
             q.next = p;
             first = q;
             return;
         }
-        IntNode k = q.next;
+        ListNode k = q.next;
         while (k != null){
             q.next = p;
             p = q;
@@ -65,12 +77,61 @@ public class SLList {
     }
 
     public void printSLList(){
-        IntNode p = this.first;
+        ListNode p = this.first;
         while(p != null){
             System.out.print(p.item);
             System.out.print(" ");
             p = p.next;
         }
         System.out.println("\n");
+    }
+
+    public Item getFirst(){
+        return this.first.item;
+    }
+
+    public Item getLast(){
+        ListNode p = this.first;
+        while (p.next != null){
+            p = p.next;
+        }
+        return p.item;
+    }
+
+    public Item removeLast(){
+        ListNode p = this.first;
+        ListNode q = p;
+        while (p.next != null){
+            q = p;
+            p = p.next;
+        }
+        Item res = p.item;
+        q.next = null;
+        return res;
+    }
+
+    public Item get(int i){
+        ListNode p = this.first;
+        while (i > 0){
+            p = p.next;
+            i -= 1;
+            if (p.next == null){
+                return null;
+            }
+        }
+        return p.item;
+    }
+
+    public int size(){
+        ListNode p = this.first;
+        if (this.first == null){
+            return 0;
+        }
+        int sizeCounter = 1;
+        while (p.next != null){
+            p = p.next;
+            sizeCounter += 1;
+        }
+        return sizeCounter;
     }
 }
--- SLListTest.java
+++ SLListTest.java
@@ -1,15 +1,18 @@
 public class SLListTest {
     public static void main(String[] args) {
-        /**
-        SLList test = new SLList(null);
-        test.addFirst(10);
-        test.addFirst(6);
-        test.addFirst(5);
-        test.addFirst(1);
-        test.insert(100, 2);
-        test.reverse();
+        /** SLList test */
+        SLList<Integer> test = new SLList<>(null);
+        for (int i = 0; i < 10; i++){
+            test.addLast(i);
+        }
+        test.addFirst(99);
+        System.out.println(test.getFirst());
+        System.out.println(test.getLast());
+        test.insert(100, 5);
+        System.out.println(test.removeLast());
         test.printSLList();
-        */
+        
+        /** AList test:
         AList<Integer> test = new AList<>();
         for (int i = 0; i < 10; i++){
             test.addLast(i);
@@ -20,5 +23,6 @@ public class SLListTest {
         test.insert(100, 5);
         System.out.println(test.removeLast());
         test.printList();
+        */
     }
 }
