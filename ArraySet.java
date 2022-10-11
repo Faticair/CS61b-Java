d3bf4409b4a21f14a96c8eb11bd58b6ea2c65484
Zahi <1737453862@qq.com>
2022-10-11 14:46:02 +0800

    override toString(), equals()

--- ArraySet.java
+++ ArraySet.java
@@ -65,15 +65,68 @@ public class ArraySet<T> implements Iterable<T> {
         return new ArraySetIterator();
     }
 
+    @Override
+    public String toString() {
+        StringBuilder returnString = new StringBuilder("{");
+        for (int i = 0; i < size - 1; i+=1){
+            returnString.append(items[i].toString());
+            returnString.append(", ");
+        }
+        returnString.append(items[size - 1].toString());
+        returnString.append("}");
+        return returnString.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj){
+            return true;
+        }
+        if (obj == null){
+            return false;
+        }
+        if (this.getClass() != obj.getClass()){
+            return false;
+        }
+
+        ArraySet<T> o = (ArraySet<T>) obj;
+        if (this.size() != o.size()){
+            return false;
+        }
+        for (T item: this){
+            if (!o.contains(item)){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         ArraySet<Integer> aset = new ArraySet<>();
         aset.add(5);
         aset.add(23);
         aset.add(42);
+        aset.add(5);
 
         //iteration
         for (int i : aset) {
             System.out.println(i);
+        }
+
+        //toString
+        System.out.println(aset.toString());
+
+        //equals
+        ArraySet<Integer> asetclone = new ArraySet<>();
+        asetclone.add(5);
+        asetclone.add(42);
+        asetclone.add(23);
+        // asetclone.add(10);
+        if (aset.equals(asetclone)){
+            System.out.println("asetclone is equal to aset.");
+        }
+        else{
+            System.out.println("asetclone is not equal to aset.");
         }
     }
 }
