e1a33e0dcd9b19a80954bc0319dc82b1695b2e26
Zahi <1737453862@qq.com>
2022-10-17 15:43:00 +0800

    BST

--- BST.java
+++ BST.java
@@ -1,4 +1,4 @@
-public class BST<Key> {
+public class BST<Key extends Comparable<Key>> {
     private Key key;
     private BST left;
     private BST right;
@@ -11,5 +11,33 @@ public class BST<Key> {
 
     public BST(Key k){
         this.key = k;
+    }
+
+    public BST search(BST T, Key k){
+        if (T == null){
+            return null;
+        }
+        if (k.equals(T.key)){
+            return T;
+        }
+        else if (k.compareTo((Key) T.key) < 0){
+            return search(T.left, k);
+        }
+        else{
+            return search(T.right, k);
+        }
+    }
+
+    public BST insert(BST T, Key k){
+        if (T == null){
+            return new BST(k);
+        }
+        if (k.compareTo((Key)T.key) < 0){
+            T.left = insert(T.left, k);
+        }
+        else if (k.compareTo((Key)T.key) > 0){
+            T.right = insert(T.right, k);
+        }
+        return T;
     }
 }
